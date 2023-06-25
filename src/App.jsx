54cf15e118fb88139d520bce4fd24b5312121a5a
Aman <amanvarshney.varshney@gmail.com>
2023-06-26 02:08:21 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <div className="generated-password">MJ7832^78bfdsa</div>
+        <button className="copy-btn" onClick={() => {}}>
+          Copy
+        </button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
